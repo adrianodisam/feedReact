0425669ee10542ed0f5089441c010e38f97528ff
Adrianodisam <adrianodisam@gmail.com>
2022-08-07 22:57:47 -0300

    envio do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './styles/App.css';
+import './styles/PostForm.css';
+import './styles/Feed.css';
 
-function App() {
+import usuarioIcone from './images/user.svg';
+import paperIcon from './images/paper-plane.svg';
+import time from './images/clock.svg';
+import empty from './images/empty-folder.svg';
+
+export default function App() {
+  const [history, setHistory] = useState('');
+  const [userName, setUserName] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  function handlesubmit(event) {
+    event.preventDefault();
+
+    setPosts([
+      ...posts,
+      {
+        id: Math.random(),
+        content: history,
+        userName,
+        dataPublica: new Date(),
+      },
+    ]);
+
+    setHistory('');
+    setUserName('');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <form className="post-form" onSubmit={handlesubmit}>
+        <input
+          value={history}
+          type="text"
+          placeholder="Escreva uma nova histtoria..."
+          onChange={(event) => {
+            setHistory(event.target.value);
+          }}
+        />
+        <div>
+          <img src={usuarioIcone} alt="Usuário" />
+          <input
+            value={userName}
+            type="text"
+            placeholder="Digite seu nome..."
+            onChange={(event) => {
+              setUserName(event.target.value);
+            }}
+          />
+          <button type="Submit">
+            <img src={paperIcon} alt="paper plane" />
+            Publicar
+          </button>
+        </div>
+      </form>
+      <main>
+        {posts.length == 0 && (
+          <div className="feed-status">
+            <img src={empty} alt="empty-folder" />
+            <h1>Não encontramos nada</h1>
+            <h2>Parece que ainda não foi postado nada </h2>
+          </div>
+        )}
+
+        {posts.length > 0 && (
+          <>
+            <header>
+              <h1>Seu feed </h1>
+              <h2>Acompanhe o que seus amgos estão pensando em tempo real</h2>
+            </header>
+            <section className="feed">
+              {posts.map((post) => (
+                <article key={post.id}>
+                  <p>{post.content}</p>
+                  <footer>
+                    <div className="user-details">
+                      <img src={usuarioIcone} alt="Usuário" />
+                      <strong>{post.userName}</strong>
+                    </div>
+                    <div className="time">
+                      <img src={time} alt="" />
+                      <span>
+                        Publicado :
+                        {post.dataPublica.toLocaleDateString('pt-br')}
+                      </span>
+                    </div>
+                  </footer>
+                </article>
+              ))}
+            </section>
+          </>
+        )}
+      </main>
     </div>
   );
 }
-
-export default App;
---
+++ src/Routes.js
@@ -0,0 +1,8 @@
+import { Routes, Route } from 'react-router-dom';
+export default function appRoutes() {
+  return (
+    <Routes>
+      <Route path="/" component={component} />
+    </Routes>
+  );
+}
